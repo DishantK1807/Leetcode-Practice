3bd240fb5202ca8a957540767fdca4ec99e8f6bf
dishantk1807 <dishant.khanna1807@gmail.com>
2020-02-26 21:46:44 -0600

    Added solution to problem 283

---
+++ 283 - Move Zeroes/JavaSolution2.java
@@ -0,0 +1,17 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int current = 0;
+        int counter = 0;
+
+        for(int i = 0; i<nums.length; i++){
+            if (nums[i] != 0){
+                nums[current] = nums[i];
+                current++;
+            }
+        }
+
+        for (int i = current; i < nums.length; i++){
+            nums[i] =0;
+        }
+    }
+}
